05cba78a809cc8e557e54fb54e914e2fde6c9c74
kasireij <32561084+kasireij@users.noreply.github.com>
2021-12-14 11:24:31 +1000

    Update welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
         @if (Route::has('login'))
         <div class="top-right links">
             @auth
-            <a href="{{ url('/home') }}">Home</a>
+            <a href="{{ route('dashboard') }}">Admin Dashboard</a>
             @else
             <a href="{{ route('login') }}">Login</a>
             @endauth
